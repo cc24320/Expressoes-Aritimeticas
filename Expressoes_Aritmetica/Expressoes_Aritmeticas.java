25ba299e4d7351f888aff91e24a3fd5c7a3a6bcf
Vitória Rosa <162496466+cc24339@users.noreply.github.com>
2025-10-07 17:34:54 -0300

    Add files via upload

---
+++ Expressoes_Aritmetica/Expressoes_Aritmeticas.java
@@ -0,0 +1,12 @@
+package Tabalho2;
+
+import java.util.StringTokenizer;
+
+public class Expressoes_Aritmeticas extends StringTokenizer{
+
+    public Expressoes_Aritmeticas(String str, String delim, boolean returnDelims) {
+        super(str, delim, returnDelims);
+    }
+    //st.nextToken();
+
+}
---
+++ Expressoes_Aritmetica/Main.java
@@ -0,0 +1,10 @@
+package Tabalho2;
+
+import java.util.StringTokenizer;
+
+public class Main {
+    
+    Expressoes_Aritmeticas st = new Expressoes_Aritmeticas("10*92+3)","()+-*|^",true);
+    st.nextToken();
+
+}
